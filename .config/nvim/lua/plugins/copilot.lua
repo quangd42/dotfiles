9b56ee6bf6a629431d67ffe22886f908f8f96221
ammon134 <ammon134@gmail.com>
2024-03-09 16:10:26 +0700

    use copilot without nvim-cmp

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,7 +1,20 @@
-return {}
--- return {
---   "zbirenbaum/copilot.lua",
---   keys = {
---     { "<leader>cc", ":Copilot suggestion toggle_auto_trigger<cr>", desc = "Toggle Copilot AutoTrigger" },
---   },
--- }
+return {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  events = "InsertEnter",
+  opts = {
+    suggestion = {
+      keymap = {
+        accept = "<M-y>",
+        accept_word = "<M-w>",
+        accept_line = "<M-l>",
+        next = "<M-n>",
+        prev = "<M-p>",
+        dismiss = "<M-Esc>",
+      },
+    },
+  },
+  keys = {
+    { "<leader>cpt", ":Copilot suggestion toggle_auto_trigger<cr>", desc = "Toggle Copilot AutoTrigger" },
+  },
+}
