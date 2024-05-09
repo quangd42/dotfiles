698f54a70f878f41101bb7355a84f5a2fea3c056
ammon134 <ammon134@gmail.com>
2024-05-09 09:46:45 +0700

    add kitty scrollback in nvim

---
+++ .config/nvim/ksb-nvim-config.lua
@@ -0,0 +1,29 @@
+-- kitty-scrollback-nvim-kitten-config.lua
+
+-- put your general Neovim configurations here
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- add kitty-scrollback.nvim to the runtimepath to allow us to require the kitty-scrollback module
+-- pick a runtimepath that corresponds with your package manager, if you are not sure leave them all it will not cause any issues
+vim.opt.runtimepath:append(vim.fn.stdpath("data") .. "/lazy/kitty-scrollback.nvim") -- lazy.nvim
+require("kitty-scrollback").setup({
+  -- put your kitty-scrollback.nvim configurations here
+})
+
+vim.opt.runtimepath:append(vim.fn.stdpath("data") .. "/lazy/tokyonight.nvim") -- lazy.nvim
+require("tokyonight").setup({ style = "moon" })
+vim.cmd([[colorscheme tokyonight]])
+
+vim.opt.runtimepath:append(vim.fn.stdpath("data") .. "/lazy/flash.nvim") -- lazy.nvim
+require("flash").setup()
+local map = vim.keymap.set
+map({ "n", "x", "o" }, "s", function()
+  require("flash").jump()
+end, { desc = "Flash" })
+map({ "n", "o", "x" }, "S", function()
+  require("flash").treesitter()
+end, { desc = "Flash Treesitter" })
+map({ "o", "x" }, "R", function()
+  require("flash").treesitter_search()
+end, { desc = "Treesitter Search" })
