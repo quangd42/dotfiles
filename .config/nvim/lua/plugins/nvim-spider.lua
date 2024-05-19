f8375c45548268dd34ff5ffa74bd22be38a6ab75
quangd42 <quang.dang.n42@gmail.com>
2024-05-19 17:18:48 -0400

    test web motion

--- .config/nvim/lua/plugins/nvim-spider.lua
+++ .config/nvim/lua/plugins/nvim-spider.lua
@@ -2,6 +2,9 @@ return {
   {
     "chrisgrieser/nvim-spider",
     lazy = true,
+    opts = {
+      skipInsignificantPunctuation = false,
+    },
     keys = {
       { "w", "<cmd>lua require('spider').motion('w')<CR>", mode = { "n", "o", "x" } },
       { "e", "<cmd>lua require('spider').motion('e')<CR>", mode = { "n", "o", "x" } },
