247f085cdf65271dc133327642b338022c4db27a
quangd42 <quang.dang.n42@gmail.com>
2024-05-19 17:19:20 -0400

    update flash

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -5,6 +5,14 @@ return {
       modes = {
         char = {
           jump_labels = true,
+          config = function(opts)
+            opts.jump_labels = opts.jump_labels
+              and vim.v.count == 0
+              and vim.fn.reg_executing() == ""
+              and vim.fn.reg_recording() == ""
+              -- Show jump labels only when not in operator-pending mode
+              and not vim.fn.mode(true):find("o")
+          end,
         },
         search = {
           enabled = true,
