5f5c6d87a7a49ba1727bbc8ce71b6efc8eb27a8a
quangd42 <quang.dang.n42@gmail.com>
2024-06-05 14:05:35 -0400

    update LazyVim

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -4,12 +4,32 @@ return {
     "hrsh7th/nvim-cmp",
     opts = {
       mapping = cmp.mapping.preset.insert({
-        ["<C-y>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        -- ["<C-y>"] = function(fallback)
+        --   if cmp.core.view:visible() or vim.fn.pumvisible() == 1 then
+        --     LazyVim.create_undo()
+        --     if cmp.confirm({ select = true }) then
+        --       return
+        --     end
+        --   end
+        --   return fallback()
+        -- end,
+        -- ["<C-a>"] = function(fallback)
+        --   if cmp.core.view:visible() or vim.fn.pumvisible() == 1 then
+        --     LazyVim.create_undo()
+        --     if
+        --       cmp.confirm({
+        --         behavior = cmp.ConfirmBehavior.Replace,
+        --         select = true,
+        --       })
+        --     then
+        --       return
+        --     end
+        --   end
+        --   return fallback()
+        -- end,
         ["<CR>"] = {},
-        ["<C-A-y>"] = cmp.mapping.confirm({
-          behavior = cmp.ConfirmBehavior.Replace,
-          select = true,
-        }),
+        ["<C-y>"] = LazyVim.cmp.confirm(),
+        ["<C-r>"] = LazyVim.cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
       }),
     },
   },
