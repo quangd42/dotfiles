966071808ed884bd306c905617c492834a2d75bd
quangd42 <quang.dang.n42@gmail.com>
2024-06-08 07:22:25 -0400

    add aerospace

--- .config/nvim/lua/plugins/mini-move.lua
+++ .config/nvim/lua/plugins/mini-move.lua
@@ -2,6 +2,21 @@ return {
   {
     "echasnovski/mini.move",
     event = "VeryLazy",
+    opts = {
+      mappings = {
+        -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+        left = "<C-M-h>",
+        right = "<C-M-l>",
+        down = "<C-M-j>",
+        up = "<C-M-k>",
+
+        -- Move current line in Normal mode
+        line_left = "<C-M-h>",
+        line_right = "<C-M-l>",
+        line_down = "<C-M-j>",
+        line_up = "<C-M-k>",
+      },
+    },
     config = function(_, opts)
       vim.schedule(function()
         require("mini.move").setup(opts)
