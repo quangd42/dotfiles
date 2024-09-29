5e280718f838ac1696da5044d836b600ac3ddf44
quangd42 <quangdn42@gmail.com>
2024-09-29 22:35:39 +0700

    use ruff as formatter instead of black

--- .config/nvim/after/ftplugin/python.lua
+++ .config/nvim/after/ftplugin/python.lua
@@ -3,5 +3,5 @@ if vim.g.vscode then
 end
 local wk = require("which-key")
 wk.add({
-  ["<leader>cV"] = { "<cmd>VenvSelectCached<cr>", "Load Last Used Venv" },
+  { "<leader>cV", "<cmd>VenvSelectCached<cr>", desc = "Load Last Used Venv" },
 })
