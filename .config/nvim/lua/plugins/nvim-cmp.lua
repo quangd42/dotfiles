9c53565abfaf4312f48e915c724ca0a0d191f105
quangd42 <quangdn42@gmail.com>
2024-10-05 14:04:37 +0700

    indentscope object to o to avoid pinky sfb

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -7,6 +7,7 @@ return {
         ["<CR>"] = {},
         ["<C-y>"] = LazyVim.cmp.confirm(),
         ["<C-e>"] = LazyVim.cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<C-m>"] = LazyVim.cmp.confirm(), -- for qwerty layout
       }),
     },
   },
