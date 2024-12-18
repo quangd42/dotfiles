445b7eaf7009316a2770cdcef7c648ff85adb88b
quangd42 <quangdn42@gmail.com>
2024-12-18 13:58:01 -0500

    blink.cmp

---
+++ .config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "saghen/blink.cmp",
+    optional = true,
+    opts = {
+      keymap = {
+        preset = "default",
+        ["<CR>"] = {},
+      },
+      signature = { enabled = true },
+    },
+  },
+}
