8c77dffb5ab01c0496a4000784f9452b3fe7e14b
quangd42 <quangdn42@gmail.com>
2024-12-18 14:01:55 -0500

    test copilot.vim

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,21 +1,52 @@
+if true then
+  return {}
+end
+local function ToggleCopilot()
+  if vim.b.copilot_enabled == nil or vim.b.copilot_enabled == true then
+    vim.b.copilot_enabled = false
+    vim.notify("Copilot disabled for current buffer", vim.log.INFO)
+  else
+    vim.b.copilot_enabled = true
+    vim.notify("Copilot enabled for current buffer", vim.log.INFO)
+  end
+end
+
+local function GloballyToggleCopilot()
+  if vim.g.copilot_enabled == nil or vim.g.copilot_enabled == true then
+    vim.cmd.Copilot("disable")
+    vim.cmd.Copilot("status")
+    vim.g.copilot_enabled = false
+  else
+    vim.cmd.Copilot("enable")
+    vim.cmd.Copilot("status")
+    vim.g.copilot_enabled = true
+  end
+end
+
 return {
-  "zbirenbaum/copilot.lua",
-  enabled = false,
-  cmd = "Copilot",
-  events = "InsertEnter",
-  opts = {
-    suggestion = {
-      keymap = {
-        accept = "<M-y>",
-        accept_word = "<M-w>",
-        accept_line = "<M-l>",
-        next = "<M-n>",
-        prev = "<M-p>",
-        dismiss = "<M-Esc>",
+  {
+    "github/copilot.vim",
+    event = "VimEnter",
+    init = function()
+      vim.g.copilot_workspace_folders = { vim.fn.getcwd() }
+      vim.g.copilot_enabled = true
+    end,
+    keys = {
+      -- { "Tab", "copilot#Accept()", { expr = true, silent = true, mode = "i" } },
+      {
+        "<leader>at",
+        function()
+          return ToggleCopilot()
+        end,
+        desc = "Toggle Copilot Suggestion in Buffer",
+      },
+      {
+        "<leader>ag",
+        function()
+          return GloballyToggleCopilot()
+        end,
+        desc = "Toggle Copilot Suggestion Globally",
       },
     },
   },
-  -- keys = {
-  --   { "<leader>ct", ":Copilot suggestion toggle_auto_trigger<cr>", desc = "Toggle Copilot AutoTrigger" },
-  -- },
 }
