08f3e7b7730e34742e1087dc37ed50e34421e69b
quangd42 <quangdn42@gmail.com>
2024-12-20 14:42:26 -0500

    nks file explorer

--- .config/nvim-kickstart/lua/plugins/oil.lua
+++ .config/nvim-kickstart/lua/plugins/oil.lua
@@ -2,6 +2,9 @@ return {
   {
     'stevearc/oil.nvim',
     opts = {
+      -- Oil will take over directory buffers (e.g. `vim .` or `:e src/`)
+      -- Set to false if you want some other plugin (e.g. netrw) to open when you edit directories.
+      default_file_explorer = true,
       -- Skip the confirmation popup for simple operations (:help oil.skip_confirm_for_simple_edits)
       skip_confirm_for_simple_edits = true,
       keymaps = {
