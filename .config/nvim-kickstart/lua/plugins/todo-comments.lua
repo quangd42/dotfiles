16fcc4ce45bbf36a35bbf88b5959565ebf5cbc8e
quangd42 <quangdn42@gmail.com>
2024-12-20 14:42:44 -0500

    nks todo comments

--- .config/nvim-kickstart/lua/plugins/todo-comments.lua
+++ .config/nvim-kickstart/lua/plugins/todo-comments.lua
@@ -1,5 +1,21 @@
--- Highlight todo, notes, etc in comments
+-- Highlight and lists all of the TODO, HACK, BUG, etc comment
+-- in your project and loads them into a browsable list.
 return {
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  {
+    'folke/todo-comments.nvim',
+    cmd = { 'TodoFzfLua' },
+    event = { 'BufReadPost', 'BufWritePost', 'BufNewFile' },
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = { signs = false },
+    -- stylua: ignore
+    keys = {
+      { "]t", function() require("todo-comments").jump_next() end, desc = "Next Todo Comment" },
+      { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous Todo Comment" },
+      -- { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
+      -- { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
+      { "<leader>st", "<cmd>TodoFzfLua<cr>", desc = "Todo" },
+      { "<leader>sT", "<cmd>TodoFzfLua keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
+    },
+  },
 }
 -- vim: ts=2 sts=2 sw=2 et
