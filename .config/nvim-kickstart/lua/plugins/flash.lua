e86dc00a7ffc84011c51ea81e35f92eebf05eedc
quangd42 <quangdn42@gmail.com>
2024-12-20 14:43:04 -0500

    nks flash

---
+++ .config/nvim-kickstart/lua/plugins/flash.lua
@@ -0,0 +1,22 @@
+return {
+  -- Flash enhances the built-in search functionality by showing labels
+  -- at the end of each match, letting you quickly jump to a specific
+  -- location.
+  {
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    vscode = true,
+    ---@type Flash.Config
+    opts = {
+      modes = { char = { highlight = { backdrop = false } } },
+    },
+    -- stylua: ignore
+    keys = {
+      { "<bs>", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "<S-bs>", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
+  },
+}
