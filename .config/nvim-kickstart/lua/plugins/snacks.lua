8ffd74b6e53e2cf62d4a4b170face3bf1eff9f26
quangd42 <quangdn42@gmail.com>
2024-12-23 18:45:27 -0500

    nks add back dashboard

--- .config/nvim-kickstart/lua/plugins/snacks.lua
+++ .config/nvim-kickstart/lua/plugins/snacks.lua
@@ -17,6 +17,18 @@ return {
       -- scope = { enabled = true },
       stypes = { enabled = true },
       words = { enabled = true },
+      dashboard = {
+        enabled = true,
+        preset = {
+          keys = {
+            { icon = ' ', key = 'f', desc = 'Find File', action = ":lua Snacks.dashboard.pick('files')" },
+            { icon = ' ', key = 'g', desc = 'Find Text', action = ":lua Snacks.dashboard.pick('live_grep')" },
+            { icon = ' ', key = 's', desc = 'Restore Session', section = 'session' },
+            { icon = '󰒲 ', key = 'l', desc = 'Lazy', action = ':Lazy', enabled = package.loaded.lazy ~= nil },
+            { icon = ' ', key = 'q', desc = 'Quit', action = ':qa' },
+          },
+        },
+      },
     },
     -- stylua: ignore start
     keys = {
