8ed5b7a91f5aae411efc8d5303d870a917e8a727
quangd42 <quangdn42@gmail.com>
2024-12-28 23:49:34 -0500

    nks conform formatter

--- .config/nvim-kickstart/lua/plugins/conform.lua
+++ .config/nvim-kickstart/lua/plugins/conform.lua
@@ -12,6 +12,14 @@ return {
         mode = '',
         desc = 'Format',
       },
+      {
+        '<leader>cF',
+        function()
+          require('conform').format { formatters = { 'injected' }, timeout_ms = 3000 }
+        end,
+        mode = { 'n', 'v' },
+        desc = 'Format Injected Langs',
+      },
     },
     opts = {
       notify_on_error = false,
@@ -33,11 +41,10 @@ return {
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        sh = { 'shfmt' },
+      },
+      formatters = {
+        injected = { options = { ignore_errors = true } },
       },
     },
   },
