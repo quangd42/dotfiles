a4d481ff685a2eb4be7aa468253416af064aebae
quangd42 <quangdn42@gmail.com>
2024-12-29 00:15:56 -0500

    nks use nonels for gomodifytags code action

--- .config/nvim-kickstart/lua/plugins/lang/go.lua
+++ .config/nvim-kickstart/lua/plugins/lang/go.lua
@@ -62,7 +62,6 @@ return {
       ensure_installed = { 'goimports', 'gofumpt', 'gomodifytags' },
     },
   },
-
   {
     'stevearc/conform.nvim',
     opts = {
@@ -70,5 +69,22 @@ return {
         go = { 'goimports', 'gofumpt' },
       },
     },
+  },
+  {
+    'nvimtools/none-ls.nvim',
+    ft = 'go',
+    dependencies = {
+      {
+        'neovim/nvim-lspconfig',
+        opts = { ensure_installed = { 'gomodifytags', 'impl' } },
+      },
+    },
+    opts = function(_, opts)
+      local nls = require 'null-ls'
+      opts.sources = vim.list_extend(opts.sources or {}, {
+        nls.builtins.code_actions.gomodifytags,
+        nls.builtins.code_actions.impl,
+      })
+    end,
   },
 }
