793a79c5d13b6f5ba7150522a45dcdc20b0f37c7
quangd42 <quangdn42@gmail.com>
2024-12-30 08:36:13 -0500

    nks search todo

--- .config/nvim-kickstart/lua/plugins/todo-comments.lua
+++ .config/nvim-kickstart/lua/plugins/todo-comments.lua
@@ -3,7 +3,8 @@
 return {
   {
     'folke/todo-comments.nvim',
-    cmd = { 'TodoFzfLua' },
+    -- cmd = { 'TodoFzfLua' },
+    cmd = { 'TodoTelescope' },
     event = { 'BufReadPost', 'BufWritePost', 'BufNewFile' },
     dependencies = { 'nvim-lua/plenary.nvim' },
     opts = { signs = false },
@@ -11,10 +12,10 @@ return {
     keys = {
       { "]t", function() require("todo-comments").jump_next() end, desc = "Next Todo Comment" },
       { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous Todo Comment" },
-      -- { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
-      -- { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
-      { "<leader>st", "<cmd>TodoFzfLua<cr>", desc = "Todo" },
-      { "<leader>sT", "<cmd>TodoFzfLua keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
+      { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
+      { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
+      -- { "<leader>st", "<cmd>TodoFzfLua<cr>", desc = "Todo" },
+      -- { "<leader>sT", "<cmd>TodoFzfLua keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
     },
   },
 }
