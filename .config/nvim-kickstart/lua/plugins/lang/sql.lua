d07f9033f664c7ef6d9884c8eb640b3c19023a81
quangd42 <quangdn42@gmail.com>
2025-01-06 16:46:25 -0500

    nks lang sql

---
+++ .config/nvim-kickstart/lua/plugins/lang/sql.lua
@@ -0,0 +1,34 @@
+local sql_ft = { 'sql', 'mysql', 'plsql' }
+return {
+  {
+    'nvim-treesitter/nvim-treesitter',
+    opts = { ensure_installed = { 'sql' } },
+  },
+
+  {
+    'stevearc/conform.nvim',
+    opts = function(_, opts)
+      -- opts.formatters.sqlfluff = {
+      --   args = { 'format', '-' },
+      -- }
+      for _, ft in ipairs(sql_ft) do
+        opts.formatters_by_ft[ft] = opts.formatters_by_ft[ft] or {}
+        table.insert(opts.formatters_by_ft[ft], 'sqlfluff')
+      end
+    end,
+  },
+
+  {
+    'williamboman/mason.nvim',
+    opts = { ensure_installed = { 'sqlfluff' } },
+  },
+
+  {
+    'nvim-treesitter/nvim-treesitter',
+    opts = function(_, opts)
+      vim.filetype.add {
+        extension = { sqlfluff = 'toml' },
+      }
+    end,
+  },
+}
