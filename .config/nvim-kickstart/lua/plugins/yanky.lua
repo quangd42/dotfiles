3ff0fcf54eda896af231db28492776374b2f2fa7
quangd42 <quangdn42@gmail.com>
2025-01-06 10:59:12 -0500

    nks add substitute

--- .config/nvim-kickstart/lua/plugins/yanky.lua
+++ .config/nvim-kickstart/lua/plugins/yanky.lua
@@ -1,32 +1,49 @@
 return {
-  'gbprod/yanky.nvim',
-  event = { 'BufReadPre', 'BufNewFile' },
-  opts = {
-    highlight = { timer = 150 },
-  },
-  keys = {
-    {
-      '<leader>p',
-      function()
-        require('telescope').extensions.yank_history.yank_history {}
-      end,
-      mode = { 'n', 'x' },
-      desc = 'Open Yank History',
+  {
+    'gbprod/yanky.nvim',
+    event = { 'BufReadPre', 'BufNewFile' },
+    opts = {
+      highlight = { timer = 150 },
     },
-    { 'y', '<Plug>(YankyYank)', mode = { 'n', 'x' }, desc = 'Yank Text' },
-    { 'p', '<Plug>(YankyPutAfter)', mode = { 'n', 'x' }, desc = 'Put Text After Cursor' },
-    { 'P', '<Plug>(YankyPutBefore)', mode = { 'n', 'x' }, desc = 'Put Text Before Cursor' },
-    { 'gp', '<Plug>(YankyGPutAfter)', mode = { 'n', 'x' }, desc = 'Put Text After Selection' },
-    { 'gP', '<Plug>(YankyGPutBefore)', mode = { 'n', 'x' }, desc = 'Put Text Before Selection' },
-    { '[y', '<Plug>(YankyCycleForward)', desc = 'Cycle Forward Through Yank History' },
-    { ']y', '<Plug>(YankyCycleBackward)', desc = 'Cycle Backward Through Yank History' },
-    { ']p', '<Plug>(YankyPutIndentAfterLinewise)', desc = 'Put Indented After Cursor (Linewise)' },
-    { '[p', '<Plug>(YankyPutIndentBeforeLinewise)', desc = 'Put Indented Before Cursor (Linewise)' },
-    { '>p', '<Plug>(YankyPutIndentAfterShiftRight)', desc = 'Put and Indent Right' },
-    { '<p', '<Plug>(YankyPutIndentAfterShiftLeft)', desc = 'Put and Indent Left' },
-    { '>P', '<Plug>(YankyPutIndentBeforeShiftRight)', desc = 'Put Before and Indent Right' },
-    { '<P', '<Plug>(YankyPutIndentBeforeShiftLeft)', desc = 'Put Before and Indent Left' },
-    { '=p', '<Plug>(YankyPutAfterFilter)', desc = 'Put After Applying a Filter' },
-    { '=P', '<Plug>(YankyPutBeforeFilter)', desc = 'Put Before Applying a Filter' },
+    keys = {
+      {
+        '<leader>p',
+        function()
+          require('telescope').extensions.yank_history.yank_history {}
+        end,
+        mode = { 'n', 'x' },
+        desc = 'Open Yank History',
+      },
+      { 'y', '<Plug>(YankyYank)', mode = { 'n', 'x' }, desc = 'Yank Text' },
+      { 'p', '<Plug>(YankyPutAfter)', mode = { 'n', 'x' }, desc = 'Put Text After Cursor' },
+      { 'P', '<Plug>(YankyPutBefore)', mode = { 'n', 'x' }, desc = 'Put Text Before Cursor' },
+      { '[y', '<Plug>(YankyCycleForward)', desc = 'Cycle Forward Through Yank History' },
+      { ']y', '<Plug>(YankyCycleBackward)', desc = 'Cycle Backward Through Yank History' },
+      { ']p', '<Plug>(YankyPutIndentAfterLinewise)', desc = 'Put Indented After Cursor (Linewise)' },
+      { '[p', '<Plug>(YankyPutIndentBeforeLinewise)', desc = 'Put Indented Before Cursor (Linewise)' },
+      { '>p', '<Plug>(YankyPutIndentAfterShiftRight)', desc = 'Put and Indent Right' },
+      { '<p', '<Plug>(YankyPutIndentAfterShiftLeft)', desc = 'Put and Indent Left' },
+      { '>P', '<Plug>(YankyPutIndentBeforeShiftRight)', desc = 'Put Before and Indent Right' },
+      { '<P', '<Plug>(YankyPutIndentBeforeShiftLeft)', desc = 'Put Before and Indent Left' },
+      { '=p', '<Plug>(YankyPutAfterFilter)', desc = 'Put After Applying a Filter' },
+      { '=P', '<Plug>(YankyPutBeforeFilter)', desc = 'Put Before Applying a Filter' },
+    },
+  },
+
+  -- allow paste to target a text object and replace it with content of register
+  {
+    'gbprod/substitute.nvim',
+    opts = {
+      highlight_substituted_text = { enabled = true, timer = 150 },
+    },
+    -- stylua: ignore
+    keys = {
+      { 'gp', function() require('substitute').operator() end, desc = 'Substitute' },
+      { 'gpp', function() require('substitute').line() end, desc = 'Substitute Line' },
+      { 'gp', function() require('substitute').visual() end, mode = { 'x' }, desc = 'Substitute' },
+      { 'gb', function() require('substitute.exchange').operator() end, desc = 'which_key_ignore' },
+      { 'gbb', function() require('substitute.exchange').line() end, desc = 'Exchange Line' },
+      { 'gb', function() require('substitute.exchange').visual() end, mode = { 'x' }, desc = 'Exchange Selection' },
+    },
   },
 }
