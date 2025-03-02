89e111571064ba187111881f53a995ff82fa26e4
quangd42 <quangdn42@gmail.com>
2025-03-02 08:40:31 -0500

    remove load_test_lsp

--- .config/nvim/after/plugin/load_test_lsp.lua
+++ .config/nvim/after/plugin/load_test_lsp.lua
@@ -1,16 +1,16 @@
-local client = vim.lsp.start_client {
-  name = 'lsp-go',
-  cmd = { '/Users/quang-dang/Workspaces/lsp-go/bin/main' },
-}
+-- local client = vim.lsp.start_client {
+--   name = 'lsp-go',
+--   cmd = { '/Users/quang-dang/Workspaces/lsp-go/bin/main' },
+-- }
 
-if not client then
-  -- vim.notify 'lsp-go server failed to start'
-  return
-end
+-- if not client then
+--   vim.notify 'lsp-go server failed to start'
+--   return
+-- end
 
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'markdown',
-  callback = function()
-    vim.lsp.buf_attach_client(0, client)
-  end,
-})
+-- vim.api.nvim_create_autocmd('FileType', {
+--   pattern = 'markdown',
+--   callback = function()
+--     vim.lsp.buf_attach_client(0, client)
+--   end,
+-- })
