8a5466363e4fc4f2f2e3137f7ab43a55a828364a
quangd42 <quangdn42@gmail.com>
2025-04-14 06:44:16 -0400

    add debugger

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -26,13 +26,43 @@ return {
   {
     'nvim-focus/focus.nvim',
     event = ui_events,
-    opts = {
-      commands = true,
-      ui = {
-        cursorline = false,
-        signcolumn = false,
-      },
-    },
+    config = function()
+      require('focus').setup {
+        commands = true,
+        ui = {
+          cursorline = false,
+          signcolumn = false,
+        },
+      }
+      local ignore_filetypes = { 'snacks_picker', 'dapui' }
+      local ignore_buftypes = { 'nofile', 'prompt', 'popup' }
+
+      local augroup = vim.api.nvim_create_augroup('FocusDisable', { clear = true })
+
+      vim.api.nvim_create_autocmd('WinEnter', {
+        group = augroup,
+        callback = function(_)
+          if vim.tbl_contains(ignore_buftypes, vim.bo.buftype) then
+            vim.w.focus_disable = true
+          else
+            vim.w.focus_disable = false
+          end
+        end,
+        desc = 'Disable focus autoresize for BufType',
+      })
+
+      vim.api.nvim_create_autocmd('FileType', {
+        group = augroup,
+        callback = function(_)
+          if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
+            vim.b.focus_disable = true
+          else
+            vim.b.focus_disable = false
+          end
+        end,
+        desc = 'Disable focus autoresize for FileType',
+      })
+    end,
     keys = {
       { '<leader>uf', '<cmd>FocusToggle<cr>', desc = 'Toggle Window Focus' },
     },
