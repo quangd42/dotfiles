ed68a1f45d559aaf00e0375a81c9df9f6414461e
quangd42 <quangdn42@gmail.com>
2025-04-19 12:15:08 -0400

    update blink to 1.

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -10,7 +10,7 @@ return {
     },
 
     -- use a release tag to download pre-built binaries
-    version = '*',
+    version = '1.*',
 
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
@@ -64,6 +64,8 @@ return {
 
       -- Experimental signature help support
       signature = { enabled = true },
+
+      fuzzy = { implementation = 'prefer_rust_with_warning' },
 
       -- Default list of enabled providers defined so that you can extend it
       -- elsewhere in your config, without redefining it, due to `opts_extend`
