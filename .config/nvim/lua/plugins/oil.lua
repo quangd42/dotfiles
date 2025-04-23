760b1979ac7721c19e61c3b64198a327ebcb36fd
quangd42 <quangdn42@gmail.com>
2025-04-23 11:18:26 -0400

    fix oil on opening dir

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -21,6 +21,25 @@ return {
     keys = {
       { '-', '<CMD>Oil --float<CR>', desc = 'Open parent directory' },
     },
+    init = function()
+      if vim.fn.argc() == 1 then
+        local stat = vim.uv.fs_stat(vim.fn.argv(0))
+        if stat and stat.type == 'directory' then
+          require('lazy').load { plugins = { 'oil.nvim' } }
+        end
+      end
+      if not require('lazy.core.config').plugins['oil.nvim']._.loaded then
+        vim.api.nvim_create_autocmd('BufNew', {
+          callback = function()
+            if vim.fn.isdirectory(vim.fn.expand '<afile>') == 1 then
+              require('lazy').load { plugins = { 'oil.nvim' } }
+              -- Once oil is loaded, we can delete this autocmd
+              return true
+            end
+          end,
+        })
+      end
+    end,
     -- Optional dependencies
     dependencies = { 'nvim-tree/nvim-web-devicons' },
   },
